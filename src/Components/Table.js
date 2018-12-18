4be92aaf53d8cde373cc22341514ed6b5ebe54b0
Michał Rutkowski <Fosternmr@gmail.com>
2018-12-19 00:47:01 +0100

    tables

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,2 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 
+
+function Table(props) {
+    return(
+        <ul>
+            <p>{props.table.id +" "+props.table.zajety}</p>
+            <button onClick={props.onPick(props.table.id)}>zamow</button>
+        </ul>
+    )
+}
---
+++ src/Components/TableList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class TableList extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            tables = []
+        }
+        this.onPick = this.onPick.bind(this);
+    }
+    
+
+    componentDidMount(){
+        axios.get("tables.json").then(res =>{
+        console.log(res);    
+        this.setState({tables: res.data});
+        });
+    }
+
+    onPick(table) {
+        event.preventDefault();
+    }
+
+    render(){
+        let tablesList = this.state.tables.map((table) => {
+            return <Table table={table} onPick={(one) => this.onPick(one)}/>
+        })
+        return tablesList;
+    }
+    
+} 
